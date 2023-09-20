3b914f5f0e38f76c1859864d75f8fe7e5045a8bf
SoWiEee <99129463+SoWiEee@users.noreply.github.com>
2023-09-20 17:04:39 +0800

    Create floatAppBar.dart

---
+++ floatAppBar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Floating App Bar';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            // 在CustomScrollView底下建立AppBar
+            const SliverAppBar(
+              title: Text(title),
+              floating: true,
+              // Display a placeholder widget to visualize the shrinking size.
+              flexibleSpace: Placeholder(),
+              // Make the initial height of the SliverAppBar larger than normal.
+              expandedHeight: 200,
+            ),
+            // 建立SliverList
+            SliverList(
+              // Use a delegate to build items as they're scrolled on screen.
+              delegate: SliverChildBuilderDelegate(
+                // The builder function returns a ListTile with a title that
+                // displays the index of the current item.
+                (context, index) => ListTile(title: Text('Item #$index')),
+                // Builds 50 ListTiles
+                childCount: 50,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
