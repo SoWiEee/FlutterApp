878fa0ab82753e9510f840d429b6f85c794011cd
SoWiEee <99129463+SoWiEee@users.noreply.github.com>
2023-09-20 16:43:46 +0800

    Create longList.dart

---
+++ longList.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(    // 建立資料源
+      items: List<String>.generate(50, (i) => 'Item $i'),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  final List<String> items;
+
+  const MyApp({super.key, required this.items});
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Long List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: ListView.builder(    // 建立為widget
+          itemCount: items.length,
+          prototypeItem: ListTile(
+            title: Text(items.first),
+          ),
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(items[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
